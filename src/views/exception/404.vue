d1c8f3c025524cfc0395ab7a3d6bf1fd133aa777
WRXinYue <3322543587@qq.com>
2023-03-25 22:34:38 +0800

    fix: 修改404页面

--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -1,20 +1,9 @@
 <template>
-  <el-row>
-    <el-col>
-      <el-result icon="warning" title="页面未找到" sub-title="请检查路径后重试或返回首页">
-        <template #extra>
-          <el-button type="primary" @click="goBack">Back</el-button>
-        </template>
-      </el-result>
-    </el-col>
-  </el-row>
+  <el-result icon="warning" title="404 NOT FOUND" sub-title="您寻找的页面已经丢失">
+    <template #extra>
+      <el-button type="primary" @click="$router.push('/')">Back</el-button>
+    </template>
+  </el-result>
 </template>
 
-<script setup lang="ts">
-const router = useRouter();
-const goBack = () => {
-  router.push({
-    name: 'home',
-  });
-};
-</script>
+<script lang="ts" setup></script>
