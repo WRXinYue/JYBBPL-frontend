1b8f4741b12c13bd105d1d01b7bc6d66f94b6030
WRXinYue <3322543587@qq.com>
2023-03-26 00:47:33 +0800

    fix: 自动进入element ui plus

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElResult: typeof import('element-plus/es')['ElResult']
