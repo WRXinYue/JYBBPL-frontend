ce5dd1b3f4fa9f6bf2a5db85d210f8115b057918
WRXinYue <3322543587@qq.com>
2023-03-26 18:48:27 +0800

    fix: 提交jQuery实例

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,8 +3,19 @@
   <h2>sss</h2>
   <h3>奈亚子吃了一个小饼干</h3>
   <h3>666666666666666666</h3>
+  <p>If you click on me, I will disappear.</p>
+  <p>Click me away!</p>
+  <p>Click me too!</p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('p').click(function () {
+    $(this).hide();
+  });
+});
+</script>
 
 <style lang="scss" scoped></style>
