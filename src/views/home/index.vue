566cf1dd12c318c9bb3d9515c06d86b5c8c9ca02
233745 <3443472383@qq.com>
2023-03-26 23:39:58 +0800

    feat：代码提交

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="a">sssssss</div>
+</template>
+
+<script setup>
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('p').click(function () {
+    $(this).hide();
+  });
+});
+</script>
+
+<style lang="scss" scoped>
+@import './css/s.scss';
+</style>
