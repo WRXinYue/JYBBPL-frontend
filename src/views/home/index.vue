7b233e568ec37cbcd262d9401221d97380358584
233745 <3443472383@qq.com>
2023-03-29 22:28:12 +0800

    fix: 修改了代码

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="a">sssssss</div>
+  <div class="container">
+    <video autoplay muted loop plays-inline class="back-video" src="./s/he1.mp4"></video>
+  </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import $ from 'jquery';
+import './js/s.js';
 
 $(document).ready(function () {
   $('p').click(function () {
@@ -14,4 +17,5 @@ $(document).ready(function () {
 
 <style lang="scss" scoped>
 @import './css/s.scss';
+@import './bootstrap-4.4.1-dist/css/bootstrap.min.css';
 </style>
